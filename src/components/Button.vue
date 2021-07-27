4d70460587cf56fd3dff101916d36000c8c1be6e
Adam Nowland <acnowland@gmail.com>
2021-07-27 14:49:04 -0700

    first vue commmit

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    color: String,
+    text: String,
+  },
+  methods: {
+    onClick() {
+      console.log("clicked");
+    },
+  },
+};
+</script>
